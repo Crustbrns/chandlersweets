f06b8a7de7f512c344e1971b08020655654708ec
Crustbrns <crustbrns@gmail.com>
2024-06-25 14:38:09 +0200

    Added player manager

--- Assets/Scripts/PC Unit.cs
+++ Assets/Scripts/PC Unit.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PCUnit : MonoBehaviour
 {
+    public GameObject unit;
+    public bool available = true;
+    public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Player Manager.cs
+++ Assets/Scripts/Player Manager.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PCManager : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
-    public List<PCUnit> Pcs;
     // Start is called before the first frame update
     void Start()
     {
